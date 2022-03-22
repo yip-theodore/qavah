45e5298fa20f59f984f632bc1a59e497f0957bc8
yip-theodore <yip-theodore@users.noreply.github.com>
2022-03-22 22:57:58 +0000

    deploy: 205012b45f733a11a4fc21ac3b73ec646554bbfc

---
+++ precache-manifest.76ceab3173b0a86cdf03ce4fb6865efd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7078415fc4c83ede046d8ffbca71e91b",
+    "url": "/qavah/index.html"
+  },
+  {
+    "revision": "49d9aa7a755e2b2bf87a",
+    "url": "/qavah/static/css/main.f46406d1.chunk.css"
+  },
+  {
+    "revision": "421a204c1f62753eb420",
+    "url": "/qavah/static/js/2.abea225b.chunk.js"
+  },
+  {
+    "revision": "5150da9ca67d846c258d3881a81903a3",
+    "url": "/qavah/static/js/2.abea225b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49d9aa7a755e2b2bf87a",
+    "url": "/qavah/static/js/main.7fc9115a.chunk.js"
+  },
+  {
+    "revision": "16deed6ddc8dfb2a021b",
+    "url": "/qavah/static/js/runtime-main.c4597c92.js"
+  }
+]);
